b6c97ca099260cff032ef0d63c1fb53f894e51f5
KamalMusayev <kmusayev038@gmail.com>
2025-08-05 15:25:09 +0000

    6-howdy_partner.py

---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Function is starting"""
+
+
+def cat_arrays(arr1, arr2):
+    """
+    Inside of Function
+    """
+    new_list = []
+    new_list.append(arr1 + arr2)
+    return new_list
