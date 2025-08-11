e22fa18b8748b62310aa991073bc1e31481d171e
KamalMusayev <kmuayev038@gmail.com>
2025-08-11 14:47:22 +0000

    4-frequency.py

---
+++ math/plotting/4-frequency.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Plotting"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def frequency():
+    """Inside of Function"""
+    np.random.seed(5)
+    student_grades = np.random.normal(68, 15, 50)
+    plt.figure(figsize=(6.4, 4.8))
+
+    bins = range(0, 101, 10)
+    plt.hist(student_grades, bins=bins, edgecolor='black')
+    plt.xlabel("Grades")
+    plt.ylabel("Number of Students")
+    plt.title("Project A")
+    plt.xticks(bins)
+    plt.show()
