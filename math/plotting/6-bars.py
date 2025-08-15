663e1bb0397ac4508a7e5f98f1ee3ca2af23e25e
KamalMusayev <kmusayev038@gmail.com>
2025-08-15 21:49:19 +0400

    Update 6-bars.py

--- math/plotting/6-bars.py
+++ math/plotting/6-bars.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
+"""Plotting"""
 import numpy as np
 import matplotlib.pyplot as plt
 
 
 def bars():
+    """Inside of Function"""
     np.random.seed(5)
     fruit = np.random.randint(0, 20, (4, 3))
     plt.figure(figsize=(6.4, 4.8))
