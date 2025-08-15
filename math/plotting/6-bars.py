27d324a5a71d32ed91507a2220d8a7fe072e9184
KamalMusayev <kmusayev038@gmail.com>
2025-08-15 21:48:39 +0400

    Update 6-bars.py

--- math/plotting/6-bars.py
+++ math/plotting/6-bars.py
@@ -2,9 +2,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def bars():
     np.random.seed(5)
-    fruit = np.random.randint(0, 20, (4,3))
+    fruit = np.random.randint(0, 20, (4, 3))
     plt.figure(figsize=(6.4, 4.8))
 
     people = ["Farrah", "Fred", "Felicia"]
