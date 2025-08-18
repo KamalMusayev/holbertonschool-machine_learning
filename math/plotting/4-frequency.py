4f915f4412496cede550329749c2ef4bfd817042
KamalMusayev <kmusayev038@gmail.com>
2025-08-18 15:28:56 +0400

    Update 4-frequency.py

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -1,20 +1,19 @@
 #!/usr/bin/env python3
-"""Plotting"""
+"""Bar graph plotting."""
 import numpy as np
 import matplotlib.pyplot as plt
 
 
 def frequency():
-    """Inside of Function"""
+    """Draw a bar graph of grade frequencies."""
     np.random.seed(5)
     student_grades = np.random.normal(68, 15, 50)
     plt.figure(figsize=(6.4, 4.8))
-
-    bins = range(0, 101, 10)
+    bins = np.arange(0, 101, 10)
     plt.hist(student_grades, bins=bins, edgecolor='black')
-    plt.axis((None, None, 0, 30))
-    plt.xlabel("Grades")
-    plt.ylabel("Number of Students")
-    plt.title("Project A")
+    plt.axis((0, 100, 0, 30))
+    plt.title('Project A')
+    plt.xlabel('Grades')
+    plt.ylabel('Number of Students')
     plt.xticks(bins)
     plt.show()
