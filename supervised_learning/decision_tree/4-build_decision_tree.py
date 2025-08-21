f5d68f8b240d7818c4d065323627a774e1a26a76
KamalMusayev <kmusayev038@gmail.com>
2025-08-21 18:53:02 +0400

    Update 4-build_decision_tree.py

--- supervised_learning/decision_tree/4-build_decision_tree.py
+++ supervised_learning/decision_tree/4-build_decision_tree.py
@@ -95,30 +95,31 @@ class Node:
             leaves.extend(self.right_child.get_leaves_below())
         return leaves
 
-    def update_bounds_below(self) :
+    def update_bounds_below(self):
         """Update Bounds"""
-        if self.is_root :
-            self.upper = { 0:np.inf }
-            self.lower = {0 : -1*np.inf }
+        if self.is_root:
+            self.lower = {0: -np.inf}
+            self.upper = {0: np.inf}
 
-        for child in [self.left_child, self.right_child] :
+        for child in [self.left_child, self.right_child]:
             if not child:
                 continue
 
             child.lower = self.lower.copy()
             child.upper = self.upper.copy()
 
-            treshhold = self.threshold
             feature = self.feature
+            threshold = self.threshold
 
             if child is self.left_child:
-                child.upper[feature] = treshhold
+                child.upper[feature] = threshold
             else:
-                child.lower[feature] = treshhold
+                child.lower[feature] = threshold
 
-        for child in [self.left_child, self.right_child] :
+        for child in [self.left_child, self.right_child]:
             if child:
-             child.update_bounds_below()
+                child.update_bounds_below()
+
 
 
 class Leaf(Node):
