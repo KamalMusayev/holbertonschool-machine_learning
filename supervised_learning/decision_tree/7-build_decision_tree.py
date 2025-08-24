48379b9711d49231294a09a6be562d0ce435efc8
KamalMusayev <kmusayev038@gmail.com>
2025-08-24 15:18:19 +0400

    Update 7-build_decision_tree.py

--- supervised_learning/decision_tree/7-build_decision_tree.py
+++ supervised_learning/decision_tree/7-build_decision_tree.py
@@ -256,12 +256,11 @@ class Decision_Tree():
         self.fit_node(self.root)
         self.update_predict()
         if verbose == 1:
-            print(f"""  Training finished.
-        - Depth                     : {self.depth()}
-        - Number of nodes           : {self.count_nodes()}
-        - Number of leaves          : {self.count_nodes(only_leaves=True)}
-        - Accuracy on training data : {self.accuracy(self.explanatory, self.target)}
-    """)
+            print(f"  Training finished.\n"
+                  f"    - Depth                     : {self.depth()}\n"
+                  f"    - Number of nodes           : {self.count_nodes()}\n"
+                  f"    - Number of leaves          : {self.count_nodes(only_leaves=True)}\n"
+                  f"    - Accuracy on training data : {self.accuracy(self.explanatory, self.target)}")
 
     def fit_node(self, node):
         """Recursively fit node and its children."""
