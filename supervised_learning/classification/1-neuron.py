2cc9e7be684b8c55539852e39bda1e285f12b93a
KamalMusayev <kmusayev038@gmail.com>
2025-08-26 18:25:35 +0400

    Update 1-neuron.py

--- supervised_learning/classification/1-neuron.py
+++ supervised_learning/classification/1-neuron.py
@@ -15,19 +15,20 @@ class Neuron:
         if nx < 1:
             raise ValueError("nx must be a positive integer")
 
-        self.W = np.random.randn(1, nx)
-        self.b = 0
-        self.A = 0
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
     @property
     def W(self):
         """Getter Method"""
         return self.__W
+
     @property
     def b(self):
         """Getter Method"""
         return self.__b
+
     @property
     def A(self):
         """Getter Method"""
         return self.__A
-
