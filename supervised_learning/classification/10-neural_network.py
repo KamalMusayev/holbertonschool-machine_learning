47e37d6492bac5022529a2ac7d12070fb1a9dd0f
KamalMusayev <kmusayev038@gmail.com>
2025-08-28 15:58:06 +0400

    Update 10-neural_network.py

--- supervised_learning/classification/10-neural_network.py
+++ supervised_learning/classification/10-neural_network.py
@@ -59,7 +59,7 @@ class NeuralNetwork:
     def forward_prop(self, X):
         """Forward Propagation"""
         Z1 = np.dot(self.__W1, X) + self.__b1
-        Z2 = np.dot(self.__W2, X) + self.__b2
         self.__A1 = 1 / (1 + np.exp(-Z1))
+        Z2 = np.dot(self.__W2, self.__A1) + self.__b2
         self.__A2 = 1 / (1 + np.exp(-Z2))
         return self.__A1, self.__A2
