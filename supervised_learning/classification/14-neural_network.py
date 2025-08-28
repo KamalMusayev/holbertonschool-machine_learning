a7942ef80e3d94f511607a53a9382cc9293d3353
KamalMusayev <kmusayev038@gmail.com>
2025-08-28 19:55:09 +0400

    Update 14-neural_network.py

--- supervised_learning/classification/14-neural_network.py
+++ supervised_learning/classification/14-neural_network.py
@@ -103,5 +103,5 @@ class NeuralNetwork:
             raise ValueError("alpha must be positive")
         for iteration in range(iterations):
             A1, A2 = self.forward_prop(X)
-            self.gradient_descent(X, Y, A2, alpha)
+            self.gradient_descent(X, Y, A1, A2, alpha)
         return self.evaluate(X, Y)
