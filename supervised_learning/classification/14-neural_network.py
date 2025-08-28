26a4291c1b56809a226af051a02a5a6d7c00b841
KamalMusayev <kmusayev038@gmail.com>
2025-08-28 19:54:01 +0400

    Update 14-neural_network.py

--- supervised_learning/classification/14-neural_network.py
+++ supervised_learning/classification/14-neural_network.py
@@ -101,7 +101,7 @@ class NeuralNetwork:
             raise TypeError("alpha must be a float")
         if alpha <= 0:
             raise ValueError("alpha must be positive")
-        for i in range(iterations):
-            self.forward_prop(X)
-            self.gradient_descent(X, Y, alpha)
+        for iteration in range(iterations):
+            A1, A2 = self.forward_prop(X)
+            self.gradient_descent(X, Y, A2, alpha)
         return self.evaluate(X, Y)
