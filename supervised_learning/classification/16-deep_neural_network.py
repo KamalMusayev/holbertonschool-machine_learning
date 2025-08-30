3bb846555c752ecde841f715e514dc6db4ecae23
KamalMusayev <kmusayev038@gmail.com>
2025-08-30 17:21:16 +0400

    Update 16-deep_neural_network.py

--- supervised_learning/classification/16-deep_neural_network.py
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -8,7 +8,7 @@ import numpy as np
 
 
 class DeepNeuralNetwork:
-    def __init__(self, nx, layers):
+   def __init__(self, nx, layers):
         """Init Function"""
         if not isinstance(nx, int):
             raise TypeError("nx must be an integer")
@@ -16,15 +16,17 @@ class DeepNeuralNetwork:
             raise ValueError("nx must be a positive integer")
         if not isinstance(layers, list) or len(layers) == 0:
             raise TypeError("layers must be a list of positive integers")
+
         self.L = len(layers)
         self.cache = {}
         self.weights = {}
         prev_nodes = nx
 
-        for l, nodes in enumerate(layers, 1):
+        for layer_idx, nodes in enumerate(layers, 1):
             if not isinstance(nodes, int) or nodes <= 0:
                 raise TypeError("layers must be a list of positive integers")
 
-            self.weights["W" + str(l)] = np.random.randn(nodes, prev_nodes) * np.sqrt(2 / prev_nodes)
-            self.weights["b" + str(l)] = np.zeros((nodes, 1))
+            self.weights["W" + str(layer_idx)] = np.random.randn(
+                nodes, prev_nodes) * np.sqrt(2 / prev_nodes)
+            self.weights["b" + str(layer_idx)] = np.zeros((nodes, 1))
             prev_nodes = nodes
