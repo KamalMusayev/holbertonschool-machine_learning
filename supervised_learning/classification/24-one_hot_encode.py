2cb7342be895480aea3769dc6ae80fe1f389f22b
KamalMusayev <kmusayev038@gmail.com>
2025-09-01 02:48:00 +0400

    Update 24-one_hot_encode.py

--- supervised_learning/classification/24-one_hot_encode.py
+++ supervised_learning/classification/24-one_hot_encode.py
@@ -7,7 +7,7 @@ import numpy as np
 
 def one_hot_encode(Y, classes):
     """One-Hot Encode Function"""
-    m = Y.shape[1]
+    m = Y.shape[0]
     one_hot = np.zeros(classes, m)
     one_hot[Y, np.arange(m)] = 1
     return one_hot
