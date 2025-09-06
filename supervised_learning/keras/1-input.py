437dc3779f5c7b104c7d91a8135bcd0058121dc2
KamalMusayev <kmusayev038@gmail.com>
2025-09-06 19:22:49 +0400

    Update 1-input.py

--- supervised_learning/keras/1-input.py
+++ supervised_learning/keras/1-input.py
@@ -2,17 +2,17 @@
 """Input Class"""
 import tensorflow.keras as K
 
-
 def build_model(nx, layers, activations, lambtha, keep_prob):
     """Build Model Function"""
-    regularizer = K.regularizer.L2(lambtha)
+    regularizer = K.regularizers.l2(lambtha)
     drop = 1 - keep_prob
-    input_model = K.layers.Input(shape=(nx,))
+    input_model = K.Input(shape=(nx,))
     x = input_model
+
     for i in range(len(layers)):
-        x = K.layers.Dense(units = layers[i],
-                           activation = activations[i],
-                           kernel_regularizer = regularizer)(x)
+        x = K.layers.Dense(units=layers[i],
+                           activation=activations[i],
+                           kernel_regularizer=regularizer)(x)
         if i < len(layers) - 1:
             x = K.layers.Dropout(rate=drop)(x)
 
