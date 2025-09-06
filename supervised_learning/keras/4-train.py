b8c648b41b157339d47ee407bee8395bf0fbbeae
KamalMusayev <kmusayev038@gmail.com>
2025-09-06 20:03:28 +0400

    Update 4-train.py

--- supervised_learning/keras/4-train.py
+++ supervised_learning/keras/4-train.py
@@ -3,9 +3,9 @@
 import tensorflow.keras as K
 
 
-def train_model(network, data, labels, batch_size, epochs, 
+def train_model(network, data, labels, batch_size, epochs,
                 verbose=True, shuffle=False):
     """Train Model"""
-    History = network.fit(data, labels, epochs=epochs,
-                          batch_size=batch_size, verbose=verbose, shuffle=shuffle)
+    History = network.fit(data, labels, epochs=epochs, batch_size=batch_size, 
+                          verbose=verbose, shuffle=shuffle)
     return History
