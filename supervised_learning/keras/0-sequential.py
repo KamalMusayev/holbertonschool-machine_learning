5cc6604548d4773cd14ffa93a9098751d385fae6
KamalMusayev <kmusayev038@gmail.com>
2025-09-06 18:52:44 +0400

    Update 0-sequential.py

--- supervised_learning/keras/0-sequential.py
+++ supervised_learning/keras/0-sequential.py
@@ -2,21 +2,21 @@
 """Classification algorithm using Deep Neural Network (DNN class)."""
 import tensorflow.keras as K
 
-
 def build_model(nx, layers, activations, lambtha, keep_prob):
     """Build Model Function"""
-    model = K.Sequential
-    model.add(K.Dense(layers[0],
-                    activation = activations[0],
-                    kernel_regularizer = K.l2(lambtha),
-                    input_shape=(nx,)))
-    model.add(K.Dropout(1 - keep_prob))
+    model = K.Sequential()
+    model.add(K.layers.Dense(layers[0],
+                             activation=activations[0],
+                             kernel_regularizer=K.regularizers.l2(lambtha),
+                             input_shape=(nx,)))
+    model.add(K.layers.Dropout(1 - keep_prob))
+    
     for i in range(1, len(layers)):
-        model.add(K.Dense(
+        model.add(K.layers.Dense(
             layers[i],
             activation=activations[i],
-            kernel_regularizer=K.l2(lambtha)
+            kernel_regularizer=K.regularizers.l2(lambtha)
         ))
         if i < len(layers) - 1:
-            model.add(K.Dropout(1 - keep_prob))
+            model.add(K.layers.Dropout(1 - keep_prob))
     return model
