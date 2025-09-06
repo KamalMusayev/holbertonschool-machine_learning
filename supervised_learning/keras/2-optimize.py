d381720ca6bb05e4ba1bc9e98ec0193d06812724
KamalMusayev <kmusayev038@gmail.com>
2025-09-06 19:38:08 +0400

    Create 2-optimize.py

---
+++ supervised_learning/keras/2-optimize.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Outside of Function"""
+import tensorflow.keras as K
+from tensorflow.python.keras.saving.saved_model.load import metrics
+
+
+def optimize_model(network, alpha, beta1, beta2):
+    """Optimize Model"""
+    optimize = K.optimizers.Adam(learning_rate=0.001, beta_1=beta1,
+                                 beta_2=beta2)
+    network.compile(loss='categorical_crossentropy',
+                    metrics=["accuracy"], optimizer=optimize)
