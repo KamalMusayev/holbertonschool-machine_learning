782ce995fded85cfed1f681cf410f02b840fb78d
KamalMusayev <kmusayev038@gmail.com>
2025-09-07 02:42:51 +0400

    Create 9-model.py

---
+++ supervised_learning/keras/9-model.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Outside of Function"""
+import tensorflow.keras as K
+
+
+def save_model(network, filename):
+    """Save Model Function"""
+    network.save(filename)
+
+def load_model(filename):
+    """Load Model Function"""
+    model = K.model.load_model(filename)
+    return model
