36e02d68920fc1238cdb5d6ba296ff53e1a47d58
KamalMusayev <kmusayev038@gmail.com>
2025-09-09 16:53:14 +0400

    Update 4-moving_average.py

--- supervised_learning/optimization/4-moving_average.py
+++ supervised_learning/optimization/4-moving_average.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
-
 """Comment of Function"""
-
 import numpy as np
 
 
 def moving_average(data, beta):
     """Moving Average"""
+    v_t = 0
     mov_avg = []
-    for i in range(len(data) - beta + 1):
-        avg = np.mean(data[i: i + beta])
-        mov_avg.append(avg)
-    return np.array(mov_avg)
+    for i in range(len(data)):
+        v_t = (beta * v_t) + ((1 - beta) * data[i])
+        bias_correction = 1 - (beta ** (i + 1))
+        mov_avg.append(v_t / bias_correction)
+    return mov_avg
