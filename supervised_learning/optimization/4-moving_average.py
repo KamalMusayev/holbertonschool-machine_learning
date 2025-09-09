40328f98c85d6ed86deecb56cfe0937da8689646
KamalMusayev <kmusayev038@gmail.com>
2025-09-09 16:30:43 +0400

    Create 4-moving_average.py

---
+++ supervised_learning/optimization/4-moving_average.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Comment of Function"""
+import numpy as np
+
+
+def moving_average(data, beta):
+    """Moving Average"""
+    mov_avg = []
+    for i in range(len(data) - beta + 1):
+        avg = np.mean(data[i: i + beta])
+        mov_avg.append(avg)
+    return np.array(mov_avg)
