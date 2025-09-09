b37114fbc0d0a6ae0ff5f0baa280d264db29253c
KamalMusayev <kmusayev038@gmail.com>
2025-09-09 19:16:57 +0400

    Create 7-RMSProp.py

---
+++ supervised_learning/optimization/7-RMSProp.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Comment of Function"""
+import numpy as np
+import tensorflow as tf
+
+
+def update_variables_RMSProp(alpha, beta2, epsilon, var, grad, s):
+    """RMSProbFunction"""
+    s = beta2 * s + (1 - beta2) * (grad ** 2)
+    var = var - alpha * grad / (np.sqrt(s) + epsilon)
+    return var, s
