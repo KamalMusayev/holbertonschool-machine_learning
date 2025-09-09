0e90f42dd61777818f9c91b1a0c4e9ec1c8796e2
KamalMusayev <kmusayev038@gmail.com>
2025-09-09 18:37:17 +0400

    Update 6-momentum.py

--- supervised_learning/optimization/6-momentum.py
+++ supervised_learning/optimization/6-momentum.py
@@ -2,6 +2,7 @@
 """Comment of Function"""
 import tensorflow as tf
 
+
 def create_momentum_op(alpha, beta1):
     """Create Momentum Optimizer"""
     optimizer = tf.keras.optimizers.SGD(learning_rate=alpha, momentum=beta1)
