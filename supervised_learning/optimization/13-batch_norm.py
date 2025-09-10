4ada823ae5f3cedd2ca6cb37fe6582bc45d05e57
KamalMusayev <kmusayev038@gmail.com>
2025-09-10 21:23:19 +0400

    Update 13-batch_norm.py

--- supervised_learning/optimization/13-batch_norm.py
+++ supervised_learning/optimization/13-batch_norm.py
@@ -7,6 +7,6 @@ def batch_norm(Z, gamma, beta, epsilon):
     """Batch Normaliztion"""
     mu = np.mean(Z, axis=0)
     var = np.var(Z, axis=0)
-    Z_ = (Z- mu) / (np.sqrt(var + epsilon))
+    Z_ = (Z - mu) / (np.sqrt(var + epsilon))
     Z_norm = gamma * Z_ + beta
     return Z_norm
