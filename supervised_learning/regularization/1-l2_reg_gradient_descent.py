9e4dc6a54054748a9a6e3eed4ecf9a63bccc29a3
KamalMusayev <kmusayev038@gmail.com>
2025-09-19 19:47:41 +0400

    Create 1-l2_reg_gradient_descent.py

---
+++ supervised_learning/regularization/1-l2_reg_gradient_descent.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Comment of Function"""
+import numpy as np
+
+
+def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
+    """L2 Regularization Gradient Descent"""
+    AL = cache['A{}'.format(L)]
+    dZ = AL - Y
+    m = Y.shape[1]
+    for i in range(L, 0, -1):
+        W = weights['W{}'.format(i)]
+        A_prev = cache['A{}'.format(i - 1)]
+        dW = (1/m) * (dZ @ A_prev.T) + (lambtha/m) * W
+        db = (1 / m) * np.sum(dZ, axis=1, keepdims=True)
+        weights['W{}'.format(i)] -= alpha * dW
+        weights['b{}'.format(i)] -= alpha * db
+        if i > 1:
+            dZ = (W.T @ dZ) * (1 - A_prev ** 2)
