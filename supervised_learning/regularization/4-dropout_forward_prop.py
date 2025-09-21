e9a107696cbb5cc54827702768f58bf897409152
KamalMusayev <kmusayev038@gmail.com>
2025-09-21 19:04:08 +0400

    Update 4-dropout_forward_prop.py

--- supervised_learning/regularization/4-dropout_forward_prop.py
+++ supervised_learning/regularization/4-dropout_forward_prop.py
@@ -7,25 +7,32 @@ def dropout_forward_prop(X, weights, L, keep_prob):
     """Dropout Forward Propagation"""
     np.random.seed(0)
     cache = {}
-    cache["A0"] = X
-    A_prev = X
+    cache['A0'] = X
+    A = X
+    # Loop for hidden layers (1 to L-1)
     for l in range(1, L):
-        W = weights["W" + str(l)]
-        b = weights["b" + str(l)]
+        A_prev = A
+        W = weights['W' + str(l)]
+        b = weights['b' + str(l)]
         Z = np.dot(W, A_prev) + b
+        # All hidden layers use tanh activation
         A = np.tanh(Z)
-
-        D = np.random.rand(A.shape[0], A.shape[1]) < keep_prob
+        # Apply dropout to the hidden layer's activation
+        D = np.random.rand(A.shape[0], A.shape[1])
+        D = (D < keep_prob).astype(int)
         A = A * D
         A = A / keep_prob
+        cache['D' + str(l)] = D
+        cache['A' + str(l)] = A
 
-        cache["A" + str(l)] = A
-        cache["D" + str(l)] = D
-        A_prev = A
+    # Calculation for the output layer (L)
+    A_prev = A
+    W = weights['W' + str(L)]
+    b = weights['b' + str(L)]
+    Z = np.dot(W, A_prev) + b
+    # The output layer uses softmax activation
+    t = np.exp(Z)
+    A = t / np.sum(t, axis=0, keepdims=True)
+    cache['A' + str(L)] = A
 
-    WL = weights["W" + str(L)]
-    bL = weights["b" + str(L)]
-    ZL = np.dot(WL, A_prev) + bL
-    AL = np.exp(ZL) / np.sum(np.exp(ZL), axis=0, keepdims=True)
-    cache["A" + str(L)] = AL
     return cache
