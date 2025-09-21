a28872d5d2df26ef8219eb706a86794042a0b4c1
KamalMusayev <kmusayev038@gmail.com>
2025-09-21 20:58:26 +0400

    Create 7-early_stopping.py

---
+++ supervised_learning/regularization/7-early_stopping.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Comment of Function"""
+import numpy as np
+
+
+def early_stopping(cost, opt_cost, threshold, patience, count):
+    """Early Stopping"""
+    if opt_cost - cost > threshold:
+        count = 0
+    else:
+        count += 1
+    return count == patience, count
