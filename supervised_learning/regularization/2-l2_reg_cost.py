a7b56e0f6a6b1bfbad16359c8fbcdc03a81c6d2f
KamalMusayev <kmusayev038@gmail.com>
2025-09-21 13:32:19 +0400

    Update 2-l2_reg_cost.py

--- supervised_learning/regularization/2-l2_reg_cost.py
+++ supervised_learning/regularization/2-l2_reg_cost.py
@@ -5,6 +5,5 @@ import tensorflow as tf
 
 def l2_reg_cost(cost, model):
     """L2 Regularization Cost"""
-    l2_loss = tf.add_n(model.losses)
-    total_costs = [cost + loss for loss in l2_loss]
+    total_costs = [cost + loss for loss in model.losses]
     return total_costs
