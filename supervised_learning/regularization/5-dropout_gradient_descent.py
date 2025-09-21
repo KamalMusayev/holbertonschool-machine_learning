d0ebdeb5cbff529e62cad7b8919eb8a8d1fdd82b
KamalMusayev <kmusayev038@gmail.com>
2025-09-21 19:54:39 +0400

    Update 5-dropout_gradient_descent.py

--- supervised_learning/regularization/5-dropout_gradient_descent.py
+++ supervised_learning/regularization/5-dropout_gradient_descent.py
@@ -12,12 +12,10 @@ def dropout_gradient_descent(Y, weights,
     dZ = AL - Y
     for i in range(L, 0, -1):
         A_prev = cache["A{}".format(i - 1)]
-        dW = (1/m) * np.dot(dZ, A_prev.T)
-        db = (1 / m) * np.sum(dZ, axis=1, keepdims=True)
-        W = W - alpha * dW
-        b = b - alpha * db
-        weights["W{}".format(i)] -= alpha * dW
-        weights["b{}".format(i)] -= alpha * db
+    dW = (1/m) * np.dot(dZ, A_prev.T)
+    db = (1/m) * np.sum(dZ, axis=1, keepdims=True)
+    weights["W{}".format(i)] -= alpha * dW
+    weights["b{}".format(i)] -= alpha * db
         if i > 1:
             W = weights["W{}".format(i)]
             dA_prev = np.dot(W.T, dZ)
