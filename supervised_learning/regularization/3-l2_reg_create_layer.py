bd4802f15e1a5339dc941b4ec09613291c453212
KamalMusayev <kmusayev038@gmail.com>
2025-09-21 14:07:57 +0400

    Update 3-l2_reg_create_layer.py

--- supervised_learning/regularization/3-l2_reg_create_layer.py
+++ supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -5,11 +5,18 @@ import tensorflow as tf
 
 def l2_reg_create_layer(prev, n, activation, lambtha):
     """L2 Regularization Create Layer"""
-    tf.random.set_seed(0)
+    # He normal başlatma üsulunu təyin edirik
+    initializer = tf.keras.initializers.HeNormal()
+    
+    # L2 requlyarizatorunu təyin edirik
     regularizer = tf.keras.regularizers.l2(lambtha)
+    
+    # Layer-i initializer və regularizer ilə yaradırıq
     output_layer = tf.keras.layers.Dense(
         units=n,
         activation=activation,
+        kernel_initializer=initializer,  # <-- Əlavə edilməli olan sətir
         kernel_regularizer=regularizer
     )(prev)
+    
     return output_layer
