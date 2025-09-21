4d102e43ecd9209108f6b6851183063fe3c70178
KamalMusayev <kmusayev038@gmail.com>
2025-09-21 13:27:03 +0400

    Create 2-l2_reg_cost.py

---
+++ supervised_learning/regularization/2-l2_reg_cost.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Comment of Function"""
+import tensorflow as tf
+
+
+def l2_reg_cost(cost, model):
+    """L2 Regularization Cost"""
+    l2_loss = tf.add_n(model.losses)
+    total_cost = cost + l2_loss
+    return total_cost
