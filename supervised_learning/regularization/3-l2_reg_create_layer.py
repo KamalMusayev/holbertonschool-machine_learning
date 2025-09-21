30ed8f5749390edd4c56c4eb8c39ab8e859a793a
KamalMusayev <kmusayev038@gmail.com>
2025-09-21 14:05:52 +0400

    Update 3-l2_reg_create_layer.py

--- supervised_learning/regularization/3-l2_reg_create_layer.py
+++ supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -5,6 +5,7 @@ import tensorflow as tf
 
 def l2_reg_create_layer(prev, n, activation, lambtha):
     """L2 Regularization Create Layer"""
+    tf.random.set_seed(0)
     regularizer = tf.keras.regularizers.l2(lambtha)
     output_layer = tf.keras.layers.Dense(
         units=n,
