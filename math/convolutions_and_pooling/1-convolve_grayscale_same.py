367f20b57d50ff48f302cc15978cc32e5dfaedaa
KamalMusayev <kmusayev038@gmail.com>
2025-09-23 19:48:18 +0400

    Update 1-convolve_grayscale_same.py

--- math/convolutions_and_pooling/1-convolve_grayscale_same.py
+++ math/convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -7,12 +7,16 @@ def convolve_grayscale_same(images, kernel):
     """Convolve Grayscale Valid"""
     m, h, w = images.shape
     kh, kw = kernel.shape
-    ph = (kh - 1) // 2
-    pw = (kw - 1) // 2
-    images_padded = np.pad(images,
-                           pad_width=((0, 0), (ph, ph), (pw, pw)),
-                           mode='constant',
-                           constant_values=0)
+    ph_top = kh // 2
+    ph_bottom = kh - 1 - ph_top
+    pw_left = kw // 2
+    pw_right = kw - 1 - pw_left
+    images_padded = np.pad(
+        images,
+        pad_width=((0, 0), (ph_top, ph_bottom), (pw_left, pw_right)),
+        mode='constant',
+        constant_values=0
+    )
     output = np.zeros((m, h, w))
     for i in range(h):
         for j in range(w):
