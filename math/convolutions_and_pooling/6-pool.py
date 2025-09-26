ac9f31bf1c55c07f26b1bb6cbb2f18be651ef1fe
KamalMusayev <kmusayev038@gmail.com>
2025-09-27 01:52:29 +0400

    Update 6-pool.py

--- math/convolutions_and_pooling/6-pool.py
+++ math/convolutions_and_pooling/6-pool.py
@@ -6,17 +6,17 @@ import numpy as np
 def pool(images, kernel_shape, stride, mode='max'):
     """Pooling"""
     m, h, w, c = images.shape
-    kh, kw, c, nc = kernel_shape.shape
+    kh, kw, c, nc = kernel_shape
     sh, sw = stride
 
     out_h = (h - kh) // sh + 1
     out_w = (w - kw) // sw + 1
-    output = np.zeros((m, out_h, out_w, nc))
+    output = np.zeros((m, out_h, out_w, c))
 
     for i in range(out_h):
         for j in range(out_w):
             image_patch = images[
-                          :, i * sh:i * sh + kh, j * sw:j * sw + kw
+                          :, i * sh:i * sh + kh, j * sw:j * sw + kw, :
                           ]
             if mode == 'max':
                 output[:, i, j, :] = np.max(image_patch, axis=(1, 2))
