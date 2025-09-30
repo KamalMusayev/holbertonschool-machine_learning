440ee656e810f9ed669b1150bb68e4fc4676159c
KamalMusayev <kmusayev038@gmail.com>
2025-09-30 19:37:37 +0400

    Update 0-conv_forward.py

--- supervised_learning/cnn/0-conv_forward.py
+++ supervised_learning/cnn/0-conv_forward.py
@@ -22,22 +22,21 @@ def conv_forward(A_prev, W, b, activation, padding="same", stride=(1, 1)):
                         ((0, 0), (ph, ph), (pw, pw), (0, 0)),
                         mode='constant', constant_values=0)
 
-
-    h_out = np.floor((h_prev + 2 * ph - kh) / sh) + 1
-    w_out = np.floor((w_prev + 2 * pw - kw) / sw) + 1
+    h_out = int(np.floor((h_prev + 2 * ph - kh) / sh) + 1)
+    w_out = int(np.floor((w_prev + 2 * pw - kw) / sw) + 1)
     output = np.zeros((m, h_out, w_out, c_new))
-    for i in range(h_out):
-        for j in range(w_out):
+    for i in range(m):
+        for h in range(h_out):
             for w in range(w_out):
                 for c in range(c_new):
-                    vert_start = h_prev * sh
+                    vert_start = j * sh
                     vert_end = vert_start + kh
-                    horiz_start = w_prev * sw
+                    horiz_start = w * sw
                     horiz_end = horiz_start + kw
-
+                    
                     slice = A_prev_pad[i, vert_start:vert_end, horiz_start:horiz_end, :]
 
-                    conv = np.sum(slice, * W[:, :, :, c] + b[:, :, :, c])
+                    conv = np.sum(slice, * W[:, :, :, c]) + b[:, :, :, c]
 
-                    output[i, j, w, c] = activation(conv)
+                    output[i, h, w, c] = activation(conv)
     return output
