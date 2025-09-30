60b8a5d47454b5a25b355c0263d9f704c90d1fe4
KamalMusayev <kmusayev038@gmail.com>
2025-09-30 19:50:27 +0400

    Update 0-conv_forward.py

--- supervised_learning/cnn/0-conv_forward.py
+++ supervised_learning/cnn/0-conv_forward.py
@@ -33,11 +33,11 @@ def conv_forward(A_prev, W, b, activation, padding="same", stride=(1, 1)):
                     horiz_start = w * sw
                     horiz_end = horiz_start + kw
 
-                    slice = A_prev_pad[i, vert_start:vert_end,
-                    horiz_start:horiz_end, :]
+                    slice_ = A_prev_pad[i, vert_start:vert_end,
+                                       horiz_start:horiz_end, :]
 
-                    conv = (np.sum(slice * W[:, :, :, c])
-                            + b[:, :, :, c])
+                    conv = (np.sum(slice_ * W[:, :, :, c])
+                            + b[0, 0, 0, c])
 
                     output[i, h, w, c] = activation(conv)
     return output
