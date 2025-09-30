676a63e3c78c5a207629c1afe609f869401aa3ce
KamalMusayev <kmusayev038@gmail.com>
2025-09-30 19:46:42 +0400

    Update 0-conv_forward.py

--- supervised_learning/cnn/0-conv_forward.py
+++ supervised_learning/cnn/0-conv_forward.py
@@ -17,17 +17,13 @@ def conv_forward(A_prev, W, b, activation, padding="same", stride=(1, 1)):
     else:
         raise ValueError("padding must be 'same' or 'valid'")
 
-    A_prev_pad = np.pad(
-        A_prev,
-        ((0, 0), (ph, ph), (pw, pw), (0, 0)),
-        mode='constant',
-        constant_values=0
-    )
+    A_prev_pad = np.pad(A_prev,
+                        ((0, 0), (ph, ph), (pw, pw), (0, 0)),
+                        mode='constant', constant_values=0)
 
     h_out = int(np.floor((h_prev + 2 * ph - kh) / sh) + 1)
     w_out = int(np.floor((w_prev + 2 * pw - kw) / sw) + 1)
     output = np.zeros((m, h_out, w_out, c_new))
-
     for i in range(m):
         for h in range(h_out):
             for w in range(w_out):
@@ -37,9 +33,11 @@ def conv_forward(A_prev, W, b, activation, padding="same", stride=(1, 1)):
                     horiz_start = w * sw
                     horiz_end = horiz_start + kw
 
-                    slice_ = A_prev_pad[i, vert_start:vert_end, horiz_start:horiz_end, :]
+                    slice = A_prev_pad[i, vert_start:vert_end,
+                    horiz_start:horiz_end, :]
 
-                    conv = np.sum(slice_ * W[:, :, :, c]) + b[:, :, :, c]
+                    conv = (np.sum(slice * W[:, :, :, c])
+                            + b[:, :, :, c])
 
                     output[i, h, w, c] = activation(conv)
     return output
