3c9f1f5f4a3126747acc30a6ea416f20add71ffd
KamalMusayev <kmusayev038@gmail.com>
2025-10-03 20:45:44 +0400

    Create 3-pool_backward.py

---
+++ supervised_learning/cnn/3-pool_backward.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""Comment of Function"""
+import numpy as np
+
+
+def pool_backward(dA, A_prev, kernel_shape, stride=(1, 1), mode='max'):
+    """Pooling Backward"""
+    m, h_new, w_new, c_new = dA.shape
+    m, h_prev, w_prev, c = A_prev.shape
+    kh, kw = kernel_shape
+    sh, sw = stride
+    dA_prev = np.zeros_like(A_prev)
+    for i in range(m):
+        for h in range(h_new):
+            for w in range(w_new):
+                for c in range(c):
+                    vert_start = h * sh
+                    vert_end = vert_start + kh
+                    horiz_start = w * sw
+                    horiz_end = horiz_start + kw
+                    slice = A_prev[i, vert_start:vert_end, horiz_start:horiz_end, c]
+                    if mode == 'max':
+                        mask = (slice == np.max(slice))
+                        dA_prev[i, vert_start:vert_end, horiz_start:horiz_end, c] += mask * dA[i, h, w, c]
+                    elif mode == 'avg':
+                        dA_prev[i, vert_start:vert_end, horiz_start:horiz_end, c] += dA[i, h, w, c] / (kh * kw)
+
+    return dA_prev
