4513aaa45b101573e053f8244bf8966024d28e32
KamalMusayev <kmusayev038@gmail.com>
2025-10-03 20:49:47 +0400

    Update 3-pool_backward.py

--- supervised_learning/cnn/3-pool_backward.py
+++ supervised_learning/cnn/3-pool_backward.py
@@ -2,27 +2,30 @@
 """Comment of Function"""
 import numpy as np
 
-
 def pool_backward(dA, A_prev, kernel_shape, stride=(1, 1), mode='max'):
     """Pooling Backward"""
     m, h_new, w_new, c_new = dA.shape
-    m, h_prev, w_prev, c = A_prev.shape
+    m, h_prev, w_prev, c_prev = A_prev.shape
     kh, kw = kernel_shape
     sh, sw = stride
+
     dA_prev = np.zeros_like(A_prev)
+
     for i in range(m):
         for h in range(h_new):
             for w in range(w_new):
-                for c in range(c):
+                for ch in range(c_new):
                     vert_start = h * sh
                     vert_end = vert_start + kh
                     horiz_start = w * sw
                     horiz_end = horiz_start + kw
-                    slice = A_prev[i, vert_start:vert_end, horiz_start:horiz_end, c]
+
+                    slice = A_prev[i, vert_start:vert_end, horiz_start:horiz_end, ch]
+
                     if mode == 'max':
                         mask = (slice == np.max(slice))
-                        dA_prev[i, vert_start:vert_end, horiz_start:horiz_end, c] += mask * dA[i, h, w, c]
+                        dA_prev[i, vert_start:vert_end, horiz_start:horiz_end, ch] += mask * dA[i, h, w, ch]
                     elif mode == 'avg':
-                        dA_prev[i, vert_start:vert_end, horiz_start:horiz_end, c] += dA[i, h, w, c] / (kh * kw)
+                        dA_prev[i, vert_start:vert_end, horiz_start:horiz_end, ch] += dA[i, h, w, ch] / (kh * kw)
 
     return dA_prev
