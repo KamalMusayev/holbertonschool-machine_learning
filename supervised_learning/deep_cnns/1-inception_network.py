83ff66ca32196016e3141d9add83adf2a07d9bab
KamalMusayev <kmusayev038@gmail.com>
2025-10-08 20:35:41 +0400

    Update 1-inception_network.py

--- supervised_learning/deep_cnns/1-inception_network.py
+++ supervised_learning/deep_cnns/1-inception_network.py
@@ -1,16 +1,11 @@
 #!/usr/bin/env python3
-"""
-Builds the inception network as described in
-Going Deeper with Convolutions (2014)
-"""
+"""Comment of Function"""
 from tensorflow import keras as K
 inception_block = __import__('0-inception_block').inception_block
 
 
 def inception_network():
-    """
-    Builds the inception network
-    """
+    """Inception Network"""
     input_layer = K.Input(shape=(224, 224, 3))
     init = K.initializers.HeNormal(seed=None)
 
