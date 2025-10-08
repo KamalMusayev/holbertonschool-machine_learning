a1e22c883a49385d8759cc4e17b9ae5c43479b08
KamalMusayev <kmusayev038@gmail.com>
2025-10-08 20:06:36 +0400

    Update 1-inception_network.py

--- supervised_learning/deep_cnns/1-inception_network.py
+++ supervised_learning/deep_cnns/1-inception_network.py
@@ -3,18 +3,54 @@
 from tensorflow import keras as K
 inception_block = __import__('0-inception_block').inception_block
 
-def inception_network(input_shape=(224, 224, 3), classes=1000):
+
+def inception_network():
     """Inception Network"""
-    input_layer = K.Input(shape=input_shape)
-    
-    
+    input_layer = K.Input(shape=(224, 224, 3))
+
     x = K.layers.Conv2D(64, (7, 7), strides=(2, 2), padding='same', activation='relu')(input_layer)
     x = K.layers.MaxPooling2D((3, 3), strides=(2, 2), padding='same')(x)
     x = K.layers.Conv2D(64, (1, 1), padding='same', activation='relu')(x)
     x = K.layers.Conv2D(192, (3, 3), strides=(1, 1), padding='same', activation='relu')(x)
     x = K.layers.MaxPooling2D((3, 3), strides=(2, 2), padding='same')(x)
-    
-    
+    x = inception_block(x, (64, 96, 128, 16, 32, 32))
+    x = inception_block(x, (128, 128, 192, 32, 96, 64))
+    x = K.layers.MaxPooling2D((3, 3), strides=(2, 2), padding='same')(x)
+    x = inception_block(x, (192, 96, 208, 16, 48, 64))
+    x = inception_block(x, (160, 112, 224, 24, 64, 64))
+    x = inception_block(x, (128, 128, 256, 24, 64, 64))
+    x = inception_block(x, (112, 144, 288, 32, 128, 128))
+    x = K.layers.MaxPooling2D((3, 3), strides=(2, 2), padding='same')(x)
+    x = inception_block(x, (256, 160, 320, 32, 128, 128))
+    x = inception_block(x, (384, 192, 384, 48, 128, 128))
+
+    x = K.layers.GlobalAveragePooling2D()(x)
+    x = K.layers.Dropout(0.4)(x)
+    output_layer = K.layers.Dense(1000, activation='softmax')(x)
+
+    model = K.Model(inputs=input_layer, outputs=output_layer)
+
+    return model
+
+
+
+#!/usr/bin/env python3
+"""Comment of Function"""
+from tensorflow import keras as K
+inception_block = __import__('0-inception_block').inception_block
+
+def inception_network(input_shape=(224, 224, 3), classes=1000):
+    """Inception Network"""
+    input_layer = K.Input(shape=input_shape)
+
+
+    x = K.layers.Conv2D(64, (7, 7), strides=(2, 2), padding='same', activation='relu')(input_layer)
+    x = K.layers.MaxPooling2D((3, 3), strides=(2, 2), padding='same')(x)
+    x = K.layers.Conv2D(64, (1, 1), padding='same', activation='relu')(x)
+    x = K.layers.Conv2D(192, (3, 3), strides=(1, 1), padding='same', activation='relu')(x)
+    x = K.layers.MaxPooling2D((3, 3), strides=(2, 2), padding='same')(x)
+
+
     x = inception_block(x, (64, 96, 128, 16, 32, 32))
     x = inception_block(x, (128, 128, 192, 32, 96, 64))
     x = K.layers.MaxPooling2D((3, 3), strides=(2, 2), padding='same')(x)
@@ -25,12 +61,24 @@ def inception_network(input_shape=(224, 224, 3), classes=1000):
     x = K.layers.MaxPooling2D((3, 3), strides=(2, 2), padding='same')(x)
     x = inception_block(x, (256, 160, 320, 32, 128, 128))
     x = inception_block(x, (384, 192, 384, 48, 128, 128))
-    
-    
+
+
     x = K.layers.GlobalAveragePooling2D()(x)
     x = K.layers.Dropout(0.4)(x)
     output_layer = K.layers.Dense(classes, activation='softmax')(x)
-    
-    model = K.Model(inputs=input_layer, outputs=output_layer, name="InceptionNetwork")
+
+    model = K.Model(inputs=input_layer, outputs=output_layer)
 
     return model
+
+
+
+
+
+
+
+
+
+
+
+
