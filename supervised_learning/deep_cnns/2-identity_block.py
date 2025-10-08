e576e6547c2bd8870ca80647c2a5a59b78c1a2fb
KamalMusayev <kmusayev038@gmail.com>
2025-10-08 21:10:37 +0400

    Create 2-identity_block.py

---
+++ supervised_learning/deep_cnns/2-identity_block.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""Comment of Function"""
+from tensorflow import keras as K
+inception_block = __import__('0-inception_block').inception_block
+
+
+def identity_block(A_prev, filters):
+    """Identity Block"""
+    F11, F3, F12 = filters
+    A_shortcut = A_prev
+    X = K.layers.Conv2D(F11, (1, 1), strides=(1, 1), padding='valid')(A_prev)
+    X = K.layers.BatchNormalization()(X)
+    X = K.layers.ReLU()(X)
+
+    X = K.layers.Conv2D(F3, (3, 3), strides=(1, 1), padding='same')(X)
+    X = K.layers.BatchNormalization()(X)
+    X = K.layers.ReLU()(X)
+
+    X = K.layers.Conv2D(F12, (1, 1), strides=(1, 1), padding='valid')(X)
+    X = K.layers.BatchNormalization()(X)
+
+    X = K.layers.Add()([X, A_shortcut])
+    X = K.layers.ReLU()(X)
+
+    return X
