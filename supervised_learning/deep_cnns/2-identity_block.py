d00fc66ea14ddccffbae10239e1720f076e8bb06
KamalMusayev <kmusayev038@gmail.com>
2025-10-10 17:20:17 +0400

    Update 2-identity_block.py

--- supervised_learning/deep_cnns/2-identity_block.py
+++ supervised_learning/deep_cnns/2-identity_block.py
@@ -3,7 +3,6 @@
 from tensorflow import keras as K
 inception_block = __import__('0-inception_block').inception_block
 
-
 def identity_block(A_prev, filters):
     """Identity Block"""
     F11, F3, F12 = filters
@@ -13,18 +12,18 @@ def identity_block(A_prev, filters):
     X = K.layers.Conv2D(F11, (1, 1), strides=(1, 1), padding='valid',
                         kernel_initializer=init)(A_prev)
     X = K.layers.BatchNormalization()(X)
-    X = K.layers.ReLU()(X)
+    X = K.layers.Activation('relu')(X)
 
     X = K.layers.Conv2D(F3, (3, 3), strides=(1, 1), padding='same',
                         kernel_initializer=init)(X)
     X = K.layers.BatchNormalization()(X)
-    X = K.layers.ReLU()(X)
+    X = K.layers.Activation('relu')(X)
 
     X = K.layers.Conv2D(F12, (1, 1), strides=(1, 1), padding='valid',
                         kernel_initializer=init)(X)
     X = K.layers.BatchNormalization()(X)
 
     X = K.layers.Add()([X, A_shortcut])
-    X = K.layers.ReLU()(X)
+    X = K.layers.Activation('relu')(X)
 
     return X
