dc73f89c9e581f96496428dbc261364c684fc399
KamalMusayev <kmusayev038@gmail.com>
2025-10-12 12:50:20 +0400

    Update 5-dense_block.py

--- supervised_learning/deep_cnns/5-dense_block.py
+++ supervised_learning/deep_cnns/5-dense_block.py
@@ -4,21 +4,21 @@ from tensorflow import keras as K
 
 
 def dense_block(X, nb_filters, growth_rate, layers):
-  """Dense Block"""
-  he_norm = K.initializers.he_normal(seed=0)
+    """Dense Block"""
+    he_norm = K.initializers.he_normal(seed=0)
 
-  for _ in range(layers):
-    out = K.layers.BatchNormalization()(X)
-    out = K.layers.Activation('relu')(out)
-    out = K.layers.Conv2D(4*growth_rate, (1,1), padding='same',
-                          kernel_initializer=he_norm)(out)
+    for _ in range(layers):
+        out = K.layers.BatchNormalization()(X)
+        out = K.layers.Activation('relu')(out)
+        out = K.layers.Conv2D(4 * growth_rate, (1, 1), padding='same',
+                              kernel_initializer=he_norm)(out)
 
-    out = K.layers.BatchNormalization()(out)
-    out = K.layers.Activation('relu')(out)
-    out = K.layers.Conv2D(growth_rate, (3,3), padding='same',
-                          kernel_initializer=he_norm)(out)
-    
-    X = K.layers.Concatenate()([X, out])
-    nb_filters += growth_rate
+        out = K.layers.BatchNormalization()(out)
+        out = K.layers.Activation('relu')(out)
+        out = K.layers.Conv2D(growth_rate, (3, 3), padding='same',
+                              kernel_initializer=he_norm)(out)
 
-  return X, nb_filters
+        X = K.layers.Concatenate()([X, out])
+        nb_filters += growth_rate
+
+    return X, nb_filters
