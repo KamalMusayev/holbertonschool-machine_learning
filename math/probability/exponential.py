8f0efe2308d118b69c4eb34619b25516f4909689
KamalMusayev <kmusayev038@gmail.com>
2025-11-12 18:38:56 +0400

    Update exponential.py

--- math/probability/exponential.py
+++ math/probability/exponential.py
@@ -5,13 +5,13 @@
 class Exponential:
     """Exponential Class"""
     def __init__(self, data=None, lambtha=1.):
-        """Initiailize Function"""
+        """Initialize Function"""
         if data is not None:
             if not isinstance(data, list):
                 raise TypeError("data must be a list")
             if len(data) < 2:
                 raise ValueError("data must contain multiple values")
-            self.lambtha = float(1 / (sum(data) / len(data))) 
+            self.lambtha = float(1 / (sum(data) / len(data)))
         else:
             if lambtha <= 0:
                 raise ValueError("lambtha must be a positive value")
