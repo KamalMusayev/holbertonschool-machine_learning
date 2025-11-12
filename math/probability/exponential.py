569b566c61cc3a938bce286ff54eaedafaddeb5a
KamalMusayev <kmusayev038@gmail.com>
2025-11-12 18:36:06 +0400

    Create exponential.py

---
+++ math/probability/exponential.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Probability"""
+
+
+class Exponential:
+    """Exponential Class"""
+    def __init__(self, data=None, lambtha=1.):
+        """Initiailize Function"""
+        if data is not None:
+            if not isinstance(data, list):
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            self.lambtha = float(sum(data) / len(data))
+        else:
+            if lambtha <= 0:
+                raise ValueError("lambtha must be a positive value")
+            self.lambtha = float(lambtha)
