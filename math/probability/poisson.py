9d3c450c9b692b00de425076737659aec5bd5035
KamalMusayev <kmusayev038@gmail.com>
2025-11-12 18:25:43 +0400

    Update poisson.py

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -30,3 +30,16 @@ class Poisson:
         for i in range(1, k + 1):
             factorial *= i
         return (self.lambtha ** k) * e_term / factorial
+
+    def cdf(self, k):
+        """Cumulative Distribution Function"""
+        if not isinstance(k, int):
+            k = int(k)
+        if k < 0:
+            return 0
+        e = 2.7182818285
+        e_term = 1 / (e ** self.lambtha)
+        factorial = 1
+        for i in range(1, k + 1):
+            factorial *= i
+        return (self.lambtha ** k) * e_term / factorial
