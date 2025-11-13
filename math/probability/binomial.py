ad0e6c5e8ec6c98af7c7a5f86395c0fd937fb071
KamalMusayev <kmusayev038@gmail.com>
2025-11-14 00:19:33 +0400

    Add Binomial class for probability analysis
    Implemented Binomial class for probability calculations.

---
+++ math/probability/binomial.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""Probability"""
+
+
+class Binomial:
+    """Binomial Class"""
+
+    def __init__(self, data=None, n=1, p=0.5):
+        """Initiailize Function"""
+        if data is not None:
+            if not isinstance(data, list):
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            mean = sum(data) / len(data)
+            var = sum((x - mean) ** 2 for x in data) / len(data)
+            self.p = 1 - (var / mean)
+            self.n = round(mean / p)
+            self.p = mean / n
+        else:
+            if n <= 0:
+                raise ValueError("n must be a positive value")
+            if not 0 <= p <= 1:
+                raise ValueError("p must be greater than 0 and less than 1")
+            self.n = int(n)
+            self.p = float(p)
