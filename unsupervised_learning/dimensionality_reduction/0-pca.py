9f4f9fc84877d299706bd06103c1af8b5f5c8efc
KamalMusayev <kmusayev038@gmail.com>
2025-11-27 21:35:52 +0000

    0-pca.py

---
+++ unsupervised_learning/dimensionality_reduction/0-pca.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""Comment of Function"""
+import numpy as np
+
+
+def pca(X, var=0.95):
+    """(Principal Component Analysis"""
+    cov = np.dot(X.T, X) / X.shape[0]
+
+    eigenvalues, eigenvectors = np.linalg.eigh(cov)
+
+    idx = np.argsort(eigenvalues)[::-1]
+    eigenvalues = eigenvalues[idx]
+    eigenvectors = eigenvectors[:, idx]
+
+    total_var = np.sum(eigenvalues)
+    cumulative_var = np.cumsum(eigenvalues) / total_var
+
+    k = np.argmax(cumulative_var >= var) + 1
+
+    W = eigenvectors[:, :k]
+
+    return W
