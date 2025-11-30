bc729ecb93b8c757a221cb920b6e2b895d9de436
KamalMusayev <kmusayev038@gmail.com>
2025-11-30 22:12:41 +0400

    Update 1-pca.py

--- unsupervised_learning/dimensionality_reduction/1-pca.py
+++ unsupervised_learning/dimensionality_reduction/1-pca.py
@@ -5,18 +5,6 @@ import numpy as np
 
 def pca(X, ndim):
     """Principal Component Analysis"""
-    X_centered = X - np.mean(X, axis=0)
-    
-    cov = np.dot(X_centered.T, X_centered) / X.shape[0]
-    
-    eigenvalues, eigenvectors = np.linalg.eigh(cov)
-    
-    idx = np.argsort(eigenvalues)[::-1]
-    eigenvectors = eigenvectors[:, idx]
-    eigenvalues = eigenvalues[idx]
-    
-    W = eigenvectors[:, :ndim]
-    
-    T = np.dot(X_centered, W)
-    
-    return T
+    X = X - np.mean(X, axis=0)
+    _, __, Vt = np.linalg.svd(X)
+    return np.matmul(X, Vt.T[..., :ndim])
