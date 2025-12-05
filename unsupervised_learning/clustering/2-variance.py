Fix indentation and add variable checks in variance.py

unsupervised_learning/clustering/2-variance.py
@@ -8,7 +8,11 @@ def variance(X, C):
       return None
   if len(X.shape) != 2 or len(C.shape) != 2:
       return None
-
+  n, d = X.shape
+  k, d_C = C.shape
+  if d != d_C:
+    return None
+    
   distances = np.linalg.norm(X[:, np.newaxis, :] - 
                              C[np.newaxis, :, :],  axis=2)
   clss = np.argmin(distances, axis=1)