b0bf0f3a37e3e32cd85092050d4c8e706a5777d1
KamalMusayev <kmusayev038@gmail.com>
2025-12-06 02:36:34 +0400

    Refactor K-means initialization and iteration logic

--- unsupervised_learning/clustering/1-kmeans.py
+++ unsupervised_learning/clustering/1-kmeans.py
@@ -3,48 +3,31 @@
 import numpy as np
 
 
-def initialize(X, k):
-    """Initializes cluster centroids for K-means"""
-    if type(X) is not np.ndarray or type(k) is not int or k <= 0:
-        return None
-
-    if len(X.shape) != 2:
-        return None
-
-    n, d = X.shape
-    if n == 0:
-        return None
-    mins = X.min(axis=0)
-    maxs = X.max(axis=0)
-    centroids = np.random.uniform(mins, maxs, size=(k, d))
-    return centroids
-
 def kmeans(X, k, iterations=1000):
     """K-means"""
-    n, d = X.shape
-    np.random.seed(0)
-    C = initialize(X, k)
-    if C is None:
+    if type(k) is not int or k <= 0:
         return None, None
-
-    clss = np.zeros(n, dtype=int)
-
-    for it in range(iterations):
-        distances = np.linalg.norm(X[:, np.newaxis, :] - C[np.newaxis, :, :], axis=2)
-        new_clss = np.argmin(distances, axis=1)
-
-        if np.array_equal(new_clss, clss):
-            break
-
-        clss = new_clss.copy()
-
-        for i in range(k):
-            points_in_cluster = X[clss == i]
-            if len(points_in_cluster) == 0:
-                mins = X.min(axis=0)
-                maxs = X.max(axis=0)
-                C[i] = np.random.uniform(mins, maxs)
+    if type(X) is not np.ndarray or len(X.shape) != 2:
+        return None, None
+    if type(iterations) is not int or iterations <= 0:
+        return None, None
+    n, d = X.shape
+    centroids = np.random.uniform(np.min(X, axis=0), np.max(X, axis=0),
+                                  size=(k, d))
+    for i in range(iterations):
+        copy = centroids.copy()
+        D = np.sqrt(((X - centroids[:, np.newaxis]) ** 2).sum(axis=2))
+        clss = np.argmin(D, axis=0)
+        for j in range(k):
+            if len(X[clss == j]) == 0:
+                centroids[j] = np.random.uniform(np.min(X, axis=0),
+                                                 np.max(X, axis=0),
+                                                 size=(1, d))
             else:
-                C[i] = points_in_cluster.mean(axis=0)
+                centroids[j] = (X[clss == j]).mean(axis=0)
+        D = np.sqrt(((X - centroids[:, np.newaxis]) ** 2).sum(axis=2))
+        clss = np.argmin(D, axis=0)
+        if np.all(copy == centroids):
+            return centroids, clss
 
-    return C, clss
+    return centroids, clss
