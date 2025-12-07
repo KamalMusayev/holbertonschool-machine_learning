2cba47644df12af074e1232062e83b39692547c3
KamalMusayev <kmusayev038@gmail.com>
2025-12-07 22:30:35 +0400

    Update 9-BIC.py

--- unsupervised_learning/clustering/9-BIC.py
+++ unsupervised_learning/clustering/9-BIC.py
@@ -19,39 +19,27 @@ def BIC(X, kmin=1, kmax=None, iterations=1000, tol=1e-5, verbose=False):
         return None, None, None, None
     if not isinstance(verbose, bool):
         return None, None, None, None
-
     n, d = X.shape
-
     if kmax is None:
         kmax = n
-
     expectation_maximization = __import__('8-EM').expectation_maximization
-
     k_range = kmax - kmin + 1
     log_likelihoods = np.zeros(k_range)
     bic_values = np.zeros(k_range)
-
     best_bic = None
     best_k = None
     best_result = None
-
     for i, k in enumerate(range(kmin, kmax + 1)):
         pi, m, S, g, log_likelihood = expectation_maximization(
             X, k, iterations, tol, verbose)
-
         if pi is None:
             return None, None, None, None
-
         log_likelihoods[i] = log_likelihood
-
         p = (k - 1) + (k * d) + (k * d * (d + 1) / 2)
-
         bic = p * np.log(n) - 2 * log_likelihood
         bic_values[i] = bic
-
         if best_bic is None or bic < best_bic:
             best_bic = bic
             best_k = k
             best_result = (pi, m, S)
-
     return best_k, best_result, log_likelihoods, bic_values
