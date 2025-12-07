fb549bb11ebb75d0c0cb8b7696ef5069c775ef86
KamalMusayev <kmusayev038@gmail.com>
2025-12-07 20:57:35 +0400

    Update 4-initialize.py

--- unsupervised_learning/clustering/4-initialize.py
+++ unsupervised_learning/clustering/4-initialize.py
@@ -7,7 +7,7 @@ kmeans = __import__('1-kmeans').kmeans
 def initialize(X, k):
     """Initializes variables for a Gaussian Mixture Model"""
     if not isinstance(X, np.ndarray) or len(X.shape) != 2 or type(k) is not int or k <= 0:
-      return None, None
+      return None, None, None
     
     pi = np.full((k, ), 1/k)
 
