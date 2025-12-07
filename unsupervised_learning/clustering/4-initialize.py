e82d68bccecea82ec772c67adcbbb14053c1f8db
KamalMusayev <kmusayev038@gmail.com>
2025-12-07 20:59:51 +0400

    Update 4-initialize.py

--- unsupervised_learning/clustering/4-initialize.py
+++ unsupervised_learning/clustering/4-initialize.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python3
-"""Comment of Function"""
+"""Initializes variables for a Gaussian Mixture Model"""
 import numpy as np
 kmeans = __import__('1-kmeans').kmeans
 
 
 def initialize(X, k):
     """Initializes variables for a Gaussian Mixture Model"""
-    if not isinstance(X, np.ndarray) or len(X.shape) != 2 or type(k) is not int or k <= 0:
-      return None, None, None
-    
-    pi = np.full((k, ), 1/k)
+    if (not isinstance(X, np.ndarray) or len(X.shape) != 2 or 
+            type(k) is not int or k <= 0):
+        return None, None, None
+
+    pi = np.full((k,), 1 / k)
 
     C, clss = kmeans(X, k)
     m = C
