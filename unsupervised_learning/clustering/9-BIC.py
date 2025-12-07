70e05d04ca3b5d5db96ca13f7633f973e7bfdec2
KamalMusayev <kmusayev038@gmail.com>
2025-12-07 22:59:09 +0400

    Refactor BIC function for clarity and efficiency

--- unsupervised_learning/clustering/9-BIC.py
+++ unsupervised_learning/clustering/9-BIC.py
@@ -1,50 +1,50 @@
 #!/usr/bin/env python3
-"""Some comments"""
+""" 0x01. Clustering """
 import numpy as np
 expectation_maximization = __import__('8-EM').expectation_maximization
 
 
 def BIC(X, kmin=1, kmax=None, iterations=1000, tol=1e-5, verbose=False):
-    """function that finds the best number of clusters for a GMM using
-    the Bayesian Information Criterion"""
-    if not isinstance(X, np.ndarray) or X.ndim != 2:
+    """
+    finds the best number of clusters for a GMM using the
+    Bayesian Information Criterion
+    """
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
         return None, None, None, None
-    if not isinstance(kmin, int) or kmin <= 0 or X.shape[0] <= kmin:
+
+    if not isinstance(kmin, int) or kmin < 1:
         return None, None, None, None
-    if kmax is None:
-        kmax = X.shape[0]
-    if not isinstance(kmax, int) or kmax <= kmin:
+
+    if not isinstance(kmax, int) or kmax < kmin:
         return None, None, None, None
-    if not isinstance(iterations, int) or iterations <= 0:
+
+    if not isinstance(iterations, int):
         return None, None, None, None
+
     if not isinstance(tol, float) or tol < 0:
         return None, None, None, None
+
     if not isinstance(verbose, bool):
         return None, None, None, None
 
-    n, d = X.shape
+    if kmax is None:
+        kmax = iterations
 
-    all_pis, all_ms, all_Ss = [], [], []
-    all_lkhds, all_bs = [], []
-
+    n = X.shape[0]
+    prior_bic = 0
+    likelyhoods = bics = []
+    best_k = kmax
+    pi_prev = m_prev = S_prev = best_res = None
     for k in range(kmin, kmax + 1):
-        pi, m, S, g, lkhd = expectation_maximization(
-            X, k, iterations, tol, verbose
-        )
-        all_pis.append(pi)
-        all_ms.append(m)
-        all_Ss.append(S)
-        all_lkhds.append(lkhd)
+        pi, m, S, g, ll = expectation_maximization(X, k, iterations, tol,
+                                                   verbose)
+        bic = k * np.log(n) - 2 * ll
+        if np.isclose(bic, prior_bic) and best_k >= k:
+            best_k = k - 1
+            best_res = pi_prev, m_prev, S_prev
+        pi_prev, m_prev, S_prev = pi, m, S
+        likelyhoods.append(ll)
+        bics.append(bic)
+        prior_bic = bic
 
-        # number of parameters
-        p = (k * d * (d + 1) / 2) + (d * k) + (k - 1)
-        b = p * np.log(n) - 2 * lkhd
-        all_bs.append(b)
-
-    all_lkhds = np.array(all_lkhds)
-    all_bs = np.array(all_bs)
-
-    best_k = np.argmin(all_bs)
-    best_result = (all_pis[best_k], all_ms[best_k], all_Ss[best_k])
-
-    return best_k + 1, best_result, all_lkhds, all_bs
+    return best_k, best_res, np.asarray(likelyhoods), np.asarray(bics)
