a33242944fe902ca97893b9ce3481bdbd09ccd72
KamalMusayev <kmusayev038@gmail.com>
2025-12-07 22:35:09 +0400

    Update 9-BIC.py

--- unsupervised_learning/clustering/9-BIC.py
+++ unsupervised_learning/clustering/9-BIC.py
@@ -1,45 +1,47 @@
 #!/usr/bin/env python3
 """Comment of Function"""
 import numpy as np
+expectation_maximization = __import__('8-EM').expectation_maximization
 
 
 def BIC(X, kmin=1, kmax=None, iterations=1000, tol=1e-5, verbose=False):
     """Finds the best number of clusters for a GMM using the BIC"""
-    if not isinstance(X, np.ndarray) or X.ndim != 2:
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
         return None, None, None, None
-    if not isinstance(kmin, int) or kmin <= 0:
+
+    if not isinstance(kmin, int) or kmin < 1:
         return None, None, None, None
-    if kmax is not None and (not isinstance(kmax, int) or kmax <= 0):
+
+    if not isinstance(kmax, int) or kmax < kmin:
         return None, None, None, None
-    if kmax is not None and kmax < kmin:
+
+    if not isinstance(iterations, int):
         return None, None, None, None
-    if not isinstance(iterations, int) or iterations <= 0:
+
+    if not isinstance(tol, float) or tol < 0:
         return None, None, None, None
-    if not isinstance(tol, (int, float)) or tol < 0:
-        return None, None, None, None
+
     if not isinstance(verbose, bool):
         return None, None, None, None
-    n, d = X.shape
+
     if kmax is None:
-        kmax = n
-    expectation_maximization = __import__('8-EM').expectation_maximization
-    k_range = kmax - kmin + 1
-    log_likelihoods = np.zeros(k_range)
-    bic_values = np.zeros(k_range)
-    best_bic = None
-    best_k = None
-    best_result = None
-    for i, k in enumerate(range(kmin, kmax + 1)):
-        pi, m, S, g, log_likelihood = expectation_maximization(
-            X, k, iterations, tol, verbose)
-        if pi is None:
-            return None, None, None, None
-        log_likelihoods[i] = log_likelihood
-        p = (k - 1) + (k * d) + (k * d * (d + 1) / 2)
-        bic = p * np.log(n) - 2 * log_likelihood
-        bic_values[i] = bic
-        if best_bic is None or bic < best_bic:
-            best_bic = bic
-            best_k = k
-            best_result = (pi, m, S)
-    return best_k, best_result, log_likelihoods, bic_values
+        kmax = iterations
+
+    n = X.shape[0]
+    prior_bic = 0
+    likelyhoods = bics = []
+    best_k = kmax
+    pi_prev = m_prev = S_prev = best_res = None
+    for k in range(kmin, kmax + 1):
+        pi, m, S, g, ll = expectation_maximization(X, k, iterations, tol,
+                                                   verbose)
+        bic = k * np.log(n) - 2 * ll
+        if np.isclose(bic, prior_bic) and best_k >= k:
+            best_k = k - 1
+            best_res = pi_prev, m_prev, S_prev
+        pi_prev, m_prev, S_prev = pi, m, S
+        likelyhoods.append(ll)
+        bics.append(bic)
+        prior_bic = bic
+
+    return best_k, best_res, np.asarray(likelyhoods), np.asarray(bics)
