e38d4a5484db24b801b7f83e3bafe63aac46e85f
KamalMusayev <kmusayev038@gmail.com>
2025-12-07 21:31:21 +0400

    Add validation for g in maximization function

--- unsupervised_learning/clustering/7-maximization.py
+++ unsupervised_learning/clustering/7-maximization.py
@@ -9,28 +9,24 @@ def maximization(X, g):
         return None, None, None
     if not isinstance(g, np.ndarray) or g.ndim != 2:
         return None, None, None
-
     n, d = X.shape
     k = g.shape[0]
-
     if g.shape[1] != n:
         return None, None, None
-
+    if not np.all(g >= 0):
+        return None, None, None
+    column_sums = np.sum(g, axis=0)
+    if not np.allclose(column_sums, 1):
+        return None, None, None
     total_prob = np.sum(g, axis=1)
-
     if np.any(total_prob == 0):
         return None, None, None
-
     pi = total_prob / n
-
     m = np.zeros((k, d))
     S = np.zeros((k, d, d))
-
     for i in range(k):
         m[i] = np.sum(g[i, :, np.newaxis] * X, axis=0) / total_prob[i]
-
         diff = X - m[i]
         S[i] = (g[i, :, np.newaxis, np.newaxis] * diff[:, :, np.newaxis] *
                 diff[:, np.newaxis, :]).sum(axis=0) / total_prob[i]
-
     return pi, m, S
