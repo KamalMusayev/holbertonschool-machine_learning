0bf2c89e077c03d80458e4bfe7ddeb0e25171ee6
KamalMusayev <kmusayev038@gmail.com>
2025-12-07 23:56:30 +0400

    Update 9-BIC.py

--- unsupervised_learning/clustering/9-BIC.py
+++ unsupervised_learning/clustering/9-BIC.py
@@ -1,15 +1,14 @@
 #!/usr/bin/env python3
-"""Some comments"""
+"""Gaussian Mixture Model implementation."""
 import numpy as np
 expectation_maximization = __import__('8-EM').expectation_maximization
 
 
 def BIC(X, kmin=1, kmax=None, iterations=1000, tol=1e-5, verbose=False):
-    """function that finds the best number of clusters for a GMM using
-    the Bayesian Information Criterion"""
-    if not isinstance(X, np.ndarray) or X.ndim != 2:
+    """Optimized version of BIC calculation"""
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
         return None, None, None, None
-    if not isinstance(kmin, int) or kmin <= 0 or X.shape[0] <= kmin:
+    if not isinstance(kmin, int) or kmin < 1:
         return None, None, None, None
     if kmax is None:
         kmax = X.shape[0]
@@ -23,28 +22,27 @@ def BIC(X, kmin=1, kmax=None, iterations=1000, tol=1e-5, verbose=False):
         return None, None, None, None
 
     n, d = X.shape
+    k_range = kmax - kmin + 1
+    L = np.zeros(k_range)
+    b = np.zeros(k_range)
+    best_k = None
+    best_result = None
+    best_BIC = np.inf
 
-    all_pis, all_ms, all_Ss = [], [], []
-    all_lkhds, all_bs = [], []
+    for i in range(kmin, kmax + 1):
+        idx = i - kmin
+        pi, m, S, g, li = expectation_maximization(
+            X, i, iterations, tol, verbose)
 
-    for k in range(kmin, kmax + 1):
-        pi, m, S, g, lkhd = expectation_maximization(
-            X, k, iterations, tol, verbose
-        )
-        all_pis.append(pi)
-        all_ms.append(m)
-        all_Ss.append(S)
-        all_lkhds.append(lkhd)
+        p = (i * d) + (i * d * (d + 1) // 2) + (i - 1)
+        current_BIC = p * np.log(n) - 2 * li
 
-        # number of parameters
-        p = (k * d * (d + 1) / 2) + (d * k) + (k - 1)
-        b = p * np.log(n) - 2 * lkhd
-        all_bs.append(b)
+        L[idx] = li
+        b[idx] = current_BIC
 
-    all_lkhds = np.array(all_lkhds)
-    all_bs = np.array(all_bs)
+        if current_BIC < best_BIC:
+            best_k = i
+            best_result = (pi, m, S)
+            best_BIC = current_BIC
 
-    best_k = np.argmin(all_bs)
-    best_result = (all_pis[best_k], all_ms[best_k], all_Ss[best_k])
-
-    return best_k + 1, best_result, all_lkhds, all_bs
+    return best_k, best_result, L, b
