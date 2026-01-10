4cba0e456858cd8229a286551c1044b7f52a77f2
KamalMusayev <kmusayev038@gmail.com>
2026-01-10 20:29:25 +0400

    Done!

--- unsupervised_learning/gan/0-simple_gan.py
+++ unsupervised_learning/gan/0-simple_gan.py
@@ -1,35 +1,62 @@
 #!/usr/bin/env python3
-"""Comment of Function"""
+"""Simple GAN Implementation"""
 import tensorflow as tf
 from tensorflow import keras
 import numpy as np
 
 
-def train_step(self, useless_argument):
-    """Train Step"""
-    for _ in range(self.disc_iter):
+class Simple_GAN(keras.Model):
+    """Simple GAN class"""
 
-        real_sample = self.get_real_sample()
-        fake_sample = self.get_fake_sample(training=True)
+    def __init__(self, generator, discriminator, latent_generator,
+                 real_examples, batch_size=200, disc_iter=2):
+        """Initialize Simple GAN"""
+        super().__init__()
+        self.latent_generator = latent_generator
+        self.real_examples = real_examples
+        self.generator = generator
+        self.discriminator = discriminator
+        self.batch_size = batch_size
+        self.disc_iter = disc_iter
+
+    def get_fake_sample(self, size=None, training=False):
+        """Generate fake samples"""
+        if not size:
+            size = self.batch_size
+        latent_sample = self.latent_generator(size)
+        return self.generator(latent_sample, training=training)
+
+    def get_real_sample(self, size=None):
+        """Get real samples"""
+        if not size:
+            size = self.batch_size
+        indices = np.random.randint(0, self.real_examples.shape[0], size)
+        return self.real_examples[indices]
+
+    def train_step(self, useless_argument):
+        """Train Step"""
+        for _ in range(self.disc_iter):
+            real_sample = self.get_real_sample()
+            fake_sample = self.get_fake_sample(training=True)
+
+            with tf.GradientTape() as tape:
+                discr_real = self.discriminator(real_sample, training=True)
+                discr_fake = self.discriminator(fake_sample, training=True)
+                discr_loss = self.discriminator.loss(discr_real, discr_fake)
+
+            grads = tape.gradient(discr_loss, self.discriminator.trainable_variables)
+            self.discriminator.optimizer.apply_gradients(
+                zip(grads, self.discriminator.trainable_variables)
+            )
 
         with tf.GradientTape() as tape:
-            discr_real = self.discriminator(real_sample, training=True)
-            discr_fake = self.discriminator(fake_sample, training=True)
-            discr_loss = self.discriminator.loss(discr_real, discr_fake)
+            fake_sample = self.get_fake_sample(training=True)
+            discr_fake = self.discriminator(fake_sample, training=False)
+            gen_loss = self.generator.loss(discr_fake)
 
-        grads = tape.gradient(discr_loss, self.discriminator.trainable_variables)
-        self.discriminator.optimizer.apply_gradients(
-            zip(grads, self.discriminator.trainable_variables)
+        grads = tape.gradient(gen_loss, self.generator.trainable_variables)
+        self.generator.optimizer.apply_gradients(
+            zip(grads, self.generator.trainable_variables)
         )
 
-    with tf.GradientTape() as tape:
-        fake_sample = self.get_fake_sample(training=True)
-        discr_fake  = self.discriminator(fake_sample, training=False)
-        gen_loss    = self.generator.loss(discr_fake)
-
-    grads = tape.gradient(gen_loss, self.generator.trainable_variables)
-    self.generator.optimizer.apply_gradients(
-        zip(grads, self.generator.trainable_variables)
-    )
-
-    return {"discr_loss": discr_loss, "gen_loss": gen_loss}
+        return {"discr_loss": discr_loss, "gen_loss": gen_loss}
