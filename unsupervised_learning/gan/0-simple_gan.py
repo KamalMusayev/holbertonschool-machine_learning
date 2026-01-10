d454bf8ef09106cac26a6894a09b799ce7826baa
KamalMusayev <kmusayev038@gmail.com>
2026-01-10 20:31:20 +0400

    Done!

--- unsupervised_learning/gan/0-simple_gan.py
+++ unsupervised_learning/gan/0-simple_gan.py
@@ -13,6 +13,15 @@ class Simple_GAN(keras.Model):
                  learning_rate=.001):
         """
         Initialize Simple GAN
+
+        Args:
+            generator: generator network
+            discriminator: discriminator network
+            latent_generator: function to generate latent space samples
+            real_examples: real data examples
+            batch_size: batch size for training
+            disc_iter: number of discriminator iterations per generator iteration
+            learning_rate: learning rate for optimizers
         """
         super().__init__()
         self.latent_generator = latent_generator
@@ -22,6 +31,7 @@ class Simple_GAN(keras.Model):
         self.batch_size = batch_size
         self.disc_iter = disc_iter
 
+        # Compile generator and discriminator with optimizers
         self.generator.compile(
             optimizer=keras.optimizers.Adam(learning_rate=learning_rate)
         )
@@ -40,8 +50,13 @@ class Simple_GAN(keras.Model):
         """Get real samples"""
         if not size:
             size = self.batch_size
-        indices = np.random.randint(0, self.real_examples.shape[0], size)
-        return self.real_examples[indices]
+        indices = tf.random.uniform(
+            shape=(size,),
+            minval=0,
+            maxval=tf.shape(self.real_examples)[0],
+            dtype=tf.int32
+        )
+        return tf.gather(self.real_examples, indices)
 
     def train_step(self, useless_argument):
         """Train Step"""
