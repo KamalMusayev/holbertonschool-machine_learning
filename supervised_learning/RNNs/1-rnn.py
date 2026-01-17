64478e8c9ae4d0de0ff84e14adf76e2a3877b575
KamalMusayev <kmusayev038@gmail.com>
2026-01-17 11:52:40 +0400

    Done!

--- supervised_learning/RNNs/1-rnn.py
+++ supervised_learning/RNNs/1-rnn.py
@@ -10,6 +10,15 @@ import numpy as np
 def rnn(rnn_cell, X, h_0):
     """
     Performs forward propagation for a simple RNN.
+
+    Parameters:
+    rnn_cell (RNNCell): Instance of RNNCell used for forward propagation
+    X (numpy.ndarray): Input data of shape (t, m, i)
+    h_0 (numpy.ndarray): Initial hidden state of shape (m, h)
+
+    Returns:
+    H (numpy.ndarray): All hidden states of shape (t, m, h)
+    Y (numpy.ndarray): All outputs of shape (t, m, o)
     """
     t, m, i = X.shape
     h = h_0.shape[1]
@@ -20,8 +29,8 @@ def rnn(rnn_cell, X, h_0):
     h_next = h_0.copy()
 
     for step in range(t):
-        H[step] = h_next
         h_next, y = rnn_cell.forward(h_next, X[step])
+        H[step] = h_next
         Y[step] = y
 
     return H, Y
