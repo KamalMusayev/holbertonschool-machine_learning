1431a2011ca58d2828cbcc8874da33972a7394f4
KamalMusayev <kmusayev038@gmail.com>
2026-01-27 17:11:10 +0400

    0-bag_of_words.py

---
+++ supervised_learning/word_embeddings/1-tf_idf.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+"""Comment of Function"""
+import numpy as np
+import math
+
+
+def tf_idf(sentences, vocab=None):
+    """TF-IDF"""
+    tokenized_sentences = [sentence.lower().split() for sentence in sentences]
+
+    if vocab is None:
+        all_words = set()
+        for tokens in tokenized_sentences:
+            all_words.update(tokens)
+        features = sorted(all_words)
+    else:
+        features = vocab
+
+    word_to_index = {word: idx for idx, word in enumerate(features)}
+
+    num_sentences = len(sentences)
+    num_features = len(features)
+
+    tf_matrix = np.zeros((num_sentences, num_features))
+
+    for i, tokens in enumerate(tokenized_sentences):
+        for token in tokens:
+            if token in word_to_index:
+                tf_matrix[i, word_to_index[token]] += 1
+
+        if len(tokens) > 0:
+            tf_matrix[i] = tf_matrix[i] / len(tokens)
+
+    idf_vector = np.zeros(num_features)
+
+    for j, word in enumerate(features):
+        doc_count = 0
+        for tokens in tokenized_sentences:
+            if word in tokens:
+                doc_count += 1
+
+        if doc_count > 0:
+            idf_vector[j] = np.log(num_sentences / doc_count)
+        else:
+            idf_vector[j] = 0
+
+    embeddings = tf_matrix * idf_vector
+
+    return embeddings, features
