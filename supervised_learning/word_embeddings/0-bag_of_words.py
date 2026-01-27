8b22b705ac3cc0009db9d64330147f01fe6afc3f
KamalMusayev <kmusayev038@gmail.com>
2026-01-27 15:53:36 +0400

    0-bag_of_words.py

---
+++ supervised_learning/word_embeddings/0-bag_of_words.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""Comment of Function"""
+import numpy as np
+
+
+def bag_of_words(sentences, vocab=None):
+    """Bag of Words"""
+    tokenized = []
+    for sentence in sentences:
+        tokens = sentence.lower().split()
+        tokenized.append(tokens)
+
+    if vocab is None:
+        features = sorted(set(word for sent in tokenized for word in sent))
+    else:
+        features = vocab
+
+    s = len(sentences)
+    f = len(features)
+    embeddings = np.zeros((s, f), dtype=int)
+
+    for i, sent in enumerate(tokenized):
+        for j, word in enumerate(features):
+            embeddings[i, j] = sent.count(word)
+
+    return embeddings, features
