6aac2393d24ce59ad3ad3546f417f3f2fe250762
KamalMusayev <kmusayev038@gmail.com>
2026-01-27 18:10:45 +0400

    0-bag_of_words.py

---
+++ supervised_learning/word_embeddings/4-fasttext.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""Comment of Function"""
+import gensim
+
+
+def fasttext_model(sentences, vector_size=100, min_count=5,
+                   negative=5, window=5, cbow=True, epochs=5, seed=0, workers=1):
+    """FastText Model"""
+    model = gensim.models.FastText(
+        sentences=sentences,
+        vector_size=vector_size,
+        min_count=min_count,
+        window=window,
+        negative=negative,
+        sg=0 if cbow else 1,
+        epochs=epochs,
+        seed=seed,
+        workers=workers
+    )
+
+    return model
