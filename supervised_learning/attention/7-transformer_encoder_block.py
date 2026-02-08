8f25e1a484ef596b174061945c0c9cb72109479e
KamalMusayev <kmusayev038@gmail.com>
2026-02-08 19:10:49 +0400

    Done!

--- supervised_learning/attention/7-transformer_encoder_block.py
+++ supervised_learning/attention/7-transformer_encoder_block.py
@@ -1,84 +1,60 @@
 #!/usr/bin/env python3
-"""Transformer Encoder Block"""
+"""Transformer Decoder Block"""
 import tensorflow as tf
 
 MultiHeadAttention = __import__('6-multihead_attention').MultiHeadAttention
 
 
-class EncoderBlock(tf.keras.layers.Layer):
-    """Encoder Block for a Transformer"""
+class DecoderBlock(tf.keras.layers.Layer):
+    """Decoder Block for a Transformer"""
 
     def __init__(self, dm, h, hidden, drop_rate=0.1):
         """
-        Initialize the Encoder Block.
-
-        Args:
-            dm: the dimensionality of the model
-            h: the number of heads
-            hidden: the number of hidden units in the fully connected layer
-            drop_rate: the dropout rate
+        Initialize the Decoder Block
         """
-        super(EncoderBlock, self).__init__()
+        super(DecoderBlock, self).__init__()
 
-        # Multi-head attention layer
-        self.mha = MultiHeadAttention(dm, h)
+        self.mha1 = MultiHeadAttention(dm, h)
 
-        # Feed-forward network with two dense layers
+        self.mha2 = MultiHeadAttention(dm, h)
+
         self.dense_hidden = tf.keras.layers.Dense(
             hidden,
             activation='relu'
         )
         self.dense_output = tf.keras.layers.Dense(dm)
 
-        # Layer normalization layers
-        self.layernorm1 = tf.keras.layers.LayerNormalization(
-            epsilon=1e-6
-        )
-        self.layernorm2 = tf.keras.layers.LayerNormalization(
-            epsilon=1e-6
-        )
+        self.layernorm1 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
+        self.layernorm2 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
+        self.layernorm3 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
 
-        # Dropout layers
         self.dropout1 = tf.keras.layers.Dropout(drop_rate)
         self.dropout2 = tf.keras.layers.Dropout(drop_rate)
+        self.dropout3 = tf.keras.layers.Dropout(drop_rate)
 
-    def call(self, x, training, mask=None):
+    def call(self, x, encoder_output, training=False, look_ahead_mask=None,
+             padding_mask=None):
         """
-        Forward pass through the encoder block.
-
-        Args:
-            x: tensor of shape (batch, input_seq_len, dm)
-               containing the input to the encoder block
-            training: boolean to determine if the model is training
-            mask: the mask to be applied for multi head attention
-
-        Returns:
-            Tensor of shape (batch, input_seq_len, dm)
-            containing the block's output
+        Forward pass through the decoder block
         """
-        # Multi-head attention
-        attn_output, _ = self.mha(x, x, x, mask)
+        attn1, _ = self.mha1(x, x, x, look_ahead_mask)
 
-        # Apply dropout
-        attn_output = self.dropout1(
-            attn_output,
-            training=training
-        )
+        attn1 = self.dropout1(attn1, training=training)
 
-        # Add & Norm
-        out1 = self.layernorm1(x + attn_output)
+        out1 = self.layernorm1(x + attn1)
 
-        # Feed-forward network
-        ffn_output = self.dense_hidden(out1)
+        attn2, _ = self.mha2(out1, encoder_output, encoder_output,
+                             padding_mask)
+
+        attn2 = self.dropout2(attn2, training=training)
+
+        out2 = self.layernorm2(out1 + attn2)
+
+        ffn_output = self.dense_hidden(out2)
         ffn_output = self.dense_output(ffn_output)
 
-        # Apply dropout
-        ffn_output = self.dropout2(
-            ffn_output,
-            training=training
-        )
+        ffn_output = self.dropout3(ffn_output, training=training)
 
-        # Add & Norm
-        out2 = self.layernorm2(out1 + ffn_output)
+        out3 = self.layernorm3(out2 + ffn_output)
 
-        return out2
+        return out3
