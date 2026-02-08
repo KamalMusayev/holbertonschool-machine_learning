0ee4d641051b80bbaa9f472d4976a5fb9df9827f
KamalMusayev <kmusayev038@gmail.com>
2026-02-08 19:08:21 +0400

    Done!

--- supervised_learning/attention/2-rnn_decoder.py
+++ supervised_learning/attention/2-rnn_decoder.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
-"""Comment of Class"""
+"""
+RNN Decoder
+"""
 
 import tensorflow as tf
 SelfAttention = __import__('1-self_attention').SelfAttention
@@ -25,17 +27,35 @@ class RNNDecoder(tf.keras.layers.Layer):
         self.attention = SelfAttention(units)
 
     def call(self, x, s_prev, hidden_states):
-        """Forward pass for the RNN decoder with attention mechanism."""
+        """
+        Forward pass for the RNN decoder with attention mechanism.
+
+        :param x: tensor, shape `(batch, 1)`, previous word in the target
+        :param s_prev: tensor, shape `(batch, units)` previous decoder
+            hidden state
+        :param hidden_states: tensor, shape `(batch, input_seq_len, units)`
+             outputs of the encoder
+
+        :return:
+        - y: tensor, shape `(batch, vocab)` output word as a one hot vector
+        in the target vocabulary
+        - s: tensor, shape `(batch, units)` new decoder hidden state
+        """
         context, _ = self.attention(s_prev, hidden_states)
 
+        # Pass the previous word index through the embedding layer
         x = self.embedding(x)
 
+        # Concatenate the context vector with x
         x = tf.concat([tf.expand_dims(context, 1), x], axis=-1)
 
+        # Pass the concatenated vector through the GRU layer
         output, s = self.gru(x)
 
+        # Remove the extra axis
         output = tf.squeeze(output, axis=1)
 
+        # Pass the GRU output through the Dense layer to predict the next word
         y = self.F(output)
 
         return y, s
--- supervised_learning/attention/7-transformer_encoder_block.py
+++ supervised_learning/attention/7-transformer_encoder_block.py
@@ -1,60 +1,84 @@
 #!/usr/bin/env python3
-"""Transformer Decoder Block"""
+"""Transformer Encoder Block"""
 import tensorflow as tf
 
 MultiHeadAttention = __import__('6-multihead_attention').MultiHeadAttention
 
 
-class DecoderBlock(tf.keras.layers.Layer):
-    """Decoder Block for a Transformer"""
+class EncoderBlock(tf.keras.layers.Layer):
+    """Encoder Block for a Transformer"""
 
     def __init__(self, dm, h, hidden, drop_rate=0.1):
         """
-        Initialize the Decoder Block
+        Initialize the Encoder Block.
+
+        Args:
+            dm: the dimensionality of the model
+            h: the number of heads
+            hidden: the number of hidden units in the fully connected layer
+            drop_rate: the dropout rate
         """
-        super(DecoderBlock, self).__init__()
+        super(EncoderBlock, self).__init__()
 
-        self.mha1 = MultiHeadAttention(dm, h)
+        # Multi-head attention layer
+        self.mha = MultiHeadAttention(dm, h)
 
-        self.mha2 = MultiHeadAttention(dm, h)
-
+        # Feed-forward network with two dense layers
         self.dense_hidden = tf.keras.layers.Dense(
             hidden,
             activation='relu'
         )
         self.dense_output = tf.keras.layers.Dense(dm)
 
-        self.layernorm1 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
-        self.layernorm2 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
-        self.layernorm3 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
+        # Layer normalization layers
+        self.layernorm1 = tf.keras.layers.LayerNormalization(
+            epsilon=1e-6
+        )
+        self.layernorm2 = tf.keras.layers.LayerNormalization(
+            epsilon=1e-6
+        )
 
+        # Dropout layers
         self.dropout1 = tf.keras.layers.Dropout(drop_rate)
         self.dropout2 = tf.keras.layers.Dropout(drop_rate)
-        self.dropout3 = tf.keras.layers.Dropout(drop_rate)
 
-    def call(self, x, encoder_output, training=False, look_ahead_mask=None,
-             padding_mask=None):
+    def call(self, x, training, mask=None):
         """
-        Forward pass through the decoder block
+        Forward pass through the encoder block.
+
+        Args:
+            x: tensor of shape (batch, input_seq_len, dm)
+               containing the input to the encoder block
+            training: boolean to determine if the model is training
+            mask: the mask to be applied for multi head attention
+
+        Returns:
+            Tensor of shape (batch, input_seq_len, dm)
+            containing the block's output
         """
-        attn1, _ = self.mha1(x, x, x, look_ahead_mask)
+        # Multi-head attention
+        attn_output, _ = self.mha(x, x, x, mask)
 
-        attn1 = self.dropout1(attn1, training=training)
+        # Apply dropout
+        attn_output = self.dropout1(
+            attn_output,
+            training=training
+        )
 
-        out1 = self.layernorm1(x + attn1)
+        # Add & Norm
+        out1 = self.layernorm1(x + attn_output)
 
-        attn2, _ = self.mha2(out1, encoder_output, encoder_output,
-                             padding_mask)
-
-        attn2 = self.dropout2(attn2, training=training)
-
-        out2 = self.layernorm2(out1 + attn2)
-
-        ffn_output = self.dense_hidden(out2)
+        # Feed-forward network
+        ffn_output = self.dense_hidden(out1)
         ffn_output = self.dense_output(ffn_output)
 
-        ffn_output = self.dropout3(ffn_output, training=training)
+        # Apply dropout
+        ffn_output = self.dropout2(
+            ffn_output,
+            training=training
+        )
 
-        out3 = self.layernorm3(out2 + ffn_output)
+        # Add & Norm
+        out2 = self.layernorm2(out1 + ffn_output)
 
-        return out3
+        return out2
