e58cd6618ee07e4de41943082c6837ba1ad7b8f4
KamalMusayev <kmusayev038@gmail.com>
2026-02-08 18:58:45 +0400

    Done!

--- supervised_learning/attention/9-transformer_encoder.py
+++ supervised_learning/attention/9-transformer_encoder.py
@@ -2,50 +2,75 @@
 """Comment of Class"""
 import tensorflow as tf
 
-positional_encoding = __import__('4-positional_encoding').positional_encoding
-EncoderBlock = __import__('7-transformer_encoder_block').EncoderBlock
+Encoder = __import__('9-transformer_encoder').Encoder
+Decoder = __import__('10-transformer_decoder').Decoder
 
 
-class Encoder(tf.keras.layers.Layer):
-    """Encoder for a Transformer"""
+class Transformer(tf.keras.Model):
+    """Transformer Network"""
 
-    def __init__(self, N, dm, h, hidden, input_vocab, max_seq_len,
-                 drop_rate=0.1):
+    def __init__(self, N, dm, h, hidden, input_vocab, target_vocab,
+                 max_seq_input, max_seq_target, drop_rate=0.1):
         """
-        Initialize the Encoder
+        Initialize the Transformer
+
+        Args:
+            N: the number of blocks in the encoder and decoder
+            dm: the dimensionality of the model
+            h: the number of heads
+            hidden: the number of hidden units in the fully connected layers
+            input_vocab: the size of the input vocabulary
+            target_vocab: the size of the target vocabulary
+            max_seq_input: the maximum sequence length possible for the input
+            max_seq_target: the maximum sequence length possible for the target
+            drop_rate: the dropout rate
         """
-        super(Encoder, self).__init__()
+        super(Transformer, self).__init__()
 
-        self.N = N
-        self.dm = dm
+        # Encoder
+        self.encoder = Encoder(N, dm, h, hidden, input_vocab,
+                               max_seq_input, drop_rate)
 
-        self.embedding = tf.keras.layers.Embedding(
-            input_dim=input_vocab,
-            output_dim=dm
-        )
+        # Decoder
+        self.decoder = Decoder(N, dm, h, hidden, target_vocab,
+                               max_seq_target, drop_rate)
 
-        self.positional_encoding = positional_encoding(max_seq_len, dm)
+        # Final linear layer to project to target vocabulary
+        self.linear = tf.keras.layers.Dense(target_vocab)
 
-        self.blocks = [EncoderBlock(dm, h, hidden, drop_rate)
-                       for _ in range(N)]
-
-        self.dropout = tf.keras.layers.Dropout(drop_rate)
-
-    def call(self, x, training=False, mask=None):
+    def call(self, inputs, target, training=False, encoder_mask=None,
+             look_ahead_mask=None, decoder_mask=None):
         """
-        Forward pass through the encoder
+        Forward pass through the transformer
+
+        Args:
+            inputs: tensor of shape (batch, input_seq_len) containing the
+                   inputs
+            target: tensor of shape (batch, target_seq_len) containing the
+                   target
+            training: boolean to determine if the model is training
+            encoder_mask: the padding mask to be applied to the encoder
+            look_ahead_mask: the look ahead mask to be applied to the decoder
+            decoder_mask: the padding mask to be applied to the decoder
+
+        Returns:
+            A tensor of shape (batch, target_seq_len, target_vocab) containing
+            the transformer output
         """
-        seq_len = x.shape[1]
+        # Pass inputs through encoder
+        # encoder_output: (batch, input_seq_len, dm)
+        encoder_output = self.encoder(inputs, training=training,
+                                      mask=encoder_mask)
 
-        x = self.embedding(x)
+        # Pass target and encoder output through decoder
+        # decoder_output: (batch, target_seq_len, dm)
+        decoder_output = self.decoder(target, encoder_output,
+                                      training=training,
+                                      look_ahead_mask=look_ahead_mask,
+                                      padding_mask=decoder_mask)
 
-        x *= tf.math.sqrt(tf.cast(self.dm, tf.float32))
+        # Apply final linear layer to project to vocabulary size
+        # output: (batch, target_seq_len, target_vocab)
+        output = self.linear(decoder_output)
 
-        x += self.positional_encoding[:seq_len, :]
-
-        x = self.dropout(x, training=training)
-
-        for block in self.blocks:
-            x = block(x, training=training, mask=mask)
-
-        return
+        return output
